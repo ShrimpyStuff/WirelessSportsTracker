8a807e611f5612d7d17af82ecb5f67b54ef9ab37
ShrimpyStuff <sajidmonowar11@gmail.com>
2022-03-11 17:43:36 -0500

    Setting up socket.io

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/site/index.html')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 })
