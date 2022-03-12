bf81cf8329a34313f9825bedea11db6e25a89778
ShrimpyStuff <sajidmonowar11@gmail.com>
2022-03-12 12:09:05 -0500

    Ok now I have dhcp setup on my "router"

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const port = 80
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/site/index.html')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
