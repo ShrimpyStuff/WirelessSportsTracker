c22020b072ca39d6970b4e71959d8c11c6a53d95
ShrimpyStuff <sajidmonowar11@gmail.com>
2022-03-12 20:35:10 -0500

    idk

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = 80
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', (req, res, next) => {
+  if (req.hostname == "admin.ranks.local") {
+    res.sendFile(__dirname + '/adminSite/index.html')
+  }
+  else if (req.ip == "192.168.16.1") {
+    res.redirect(`${req.ip}:3000`)
+  }
+  else if (req.hostname == "status") {
+    res.redirect(`${req.hostname}:3000`)
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/site/index.html')
+  res.sendFile(__dirname + '/public/site/index.html')
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('lookup', (input) => {
+    console.log(input)
   });
 });
 
