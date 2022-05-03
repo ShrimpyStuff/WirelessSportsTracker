319aacc1530bd4765513df842df1c7a28b7a2fb2
Sajid Monowar <sajidmonowar11@gmail.com>
2022-05-03 13:56:55 -0400

    Idk make it look better

--- public/site/index.js
+++ public/site/index.js
@@ -16,38 +16,38 @@ if (input.value) {
 });
 
 monitor.addEventListener('click', (e) => {
-socket.emit('monitorRequest');
+    socket.emit('monitorRequest');
 });
 
 socket.on('infoReturn', (json) => {
-monitor.disabled = false;
-let jsonParse = JSON.parse(JSON.parse(JSON.stringify(json)));
-if (!Array.isArray(jsonParse)) {
-    jsonParse = [];
-    jsonParse.push(JSON.parse(JSON.stringify(json)));
-}
-for (let dataObject of jsonParse) {
-    document.getElementById('name').textContent = dataObject.person;
-    document.getElementById('grade').textContent = dataObject.grade;
-    document.getElementById('dob').textContent = dataObject.dob;
-    let event = document.createElement('tr');
-
-    let title = document.createElement('th');
-    title.appendChild(document.createTextNode(dataObject.title));
-    event.appendChild(title);
-
-    let time = document.createElement('td');
-    time.appendChild(document.createTextNode(dataObject.time));
-    event.appendChild(time);
-
-    let placing = document.createElement('td');
-    if (dataObject.finished == true) {
-        placing.appendChild(document.createTextNode(dataObject.placing));
+    monitor.disabled = false;
+    let jsonParse = JSON.parse(JSON.parse(JSON.stringify(json)));
+    if (!Array.isArray(jsonParse)) {
+        jsonParse = [];
+        jsonParse.push(JSON.parse(JSON.stringify(json)));
     }
-    else if (dataObject.finished != true) {
-        placing.appendChild(document.createTextNode("Event not finished or Error"));
+    for (let dataObject of jsonParse) {
+        document.getElementById('name').textContent = dataObject.person;
+        document.getElementById('grade').textContent = dataObject.grade;
+        document.getElementById('dob').textContent = dataObject.dob;
+        let event = document.createElement('tr');
+
+        let title = document.createElement('th');
+        title.appendChild(document.createTextNode(dataObject.title));
+        event.appendChild(title);
+
+        let time = document.createElement('td');
+        time.appendChild(document.createTextNode(dataObject.time));
+        event.appendChild(time);
+
+        let placing = document.createElement('td');
+        if (dataObject.finished == true) {
+            placing.appendChild(document.createTextNode(dataObject.placing));
+        }
+        else if (dataObject.finished != true) {
+            placing.appendChild(document.createTextNode("Event not finished or Error"));
+        }
+        event.appendChild(placing);
+        table.appendChild(event);
     }
-    event.appendChild(placing);
-    table.appendChild(event);
-}
 });
