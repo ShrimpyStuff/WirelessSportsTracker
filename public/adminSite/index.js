0d506f9517b723f01b7539a6ab864fe489309b7e
Sajid Monowar <sajidmonowar11@gmail.com>
2022-05-05 14:15:32 -0400

    Part of the text editable stuff things

--- public/adminSite/index.js
+++ public/adminSite/index.js
@@ -1,1 +1,47 @@
-let socket = io();
+//let socket = io();
+let nameCaption = document.getElementById('nameText');
+let gradeCaption = document.getElementById('gradeText');
+let dobCaption = document.getElementById('dobText');
+
+document.getElementById("Lookup").addEventListener('click', (e) => {
+    console.log(nameCaption.textContent.trim());
+    console.log(gradeCaption.textContent.trim());
+    console.log(dobCaption.textContent.trim());
+    socket.emit('lookup', nameCaption.textContent().trim());
+});
+
+socket.on('infoReturn', (json) => {
+    let jsonParse = JSON.parse(JSON.parse(JSON.stringify(json)));
+    if (!Array.isArray(jsonParse)) {
+        jsonParse = [];
+        jsonParse.push(JSON.parse(JSON.stringify(json)));
+    }
+    for (let dataObject of jsonParse) {
+        nameCaption.textContent = dataObject.person;
+        gradeCaption.textContent = dataObject.grade;
+        dobCaption.textContent = dataObject.dob;
+        let event = document.createElement('tr');
+
+        let title = document.createElement('th');
+        let titleTextNode = document.createTextNode(dataObject.title);
+        titleTextNode.contentEditable = true;
+        title.appendChild(titleTextNode);
+        event.appendChild(title);
+
+        let time = document.createElement('td');
+        let timeTextNode = document.createTextNode(dataObject.time);
+        timeTextNode.contentEditable = true;
+        time.appendChild(timeTextNode);
+        event.appendChild(time);
+
+        let placing = document.createElement('td');
+        if (dataObject.finished == true) {
+            placing.appendChild(document.createTextNode(dataObject.placing));
+        }
+        else {
+            placing.appendChild(document.createTextNode("Event not finished or Error"));
+        }
+        event.appendChild(placing);
+        table.appendChild(event);
+    }
+});
