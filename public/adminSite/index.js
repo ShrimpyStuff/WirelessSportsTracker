8020085ab8d96f89dc098d17edab3e0af9eb0dd8
ShrimpyStuff <sajidmonowar11@gmail.com>
2022-05-08 19:16:31 -0400

    Some sort of update

--- public/adminSite/index.js
+++ public/adminSite/index.js
@@ -1,13 +1,37 @@
 let socket = io();
+let table = document.getElementById('table');
 let nameCaption = document.getElementById('nameText');
 let gradeCaption = document.getElementById('gradeText');
 let dobCaption = document.getElementById('dobText');
 
-document.getElementById("Lookup").addEventListener('click', (e) => {
+document.getElementById('Lookup').addEventListener('click', (e) => {
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
     socket.emit('lookup', nameCaption.textContent.trim());
+});
+
+document.getElementById('newEventRow').addEventListener('click', (e) => {
+    let event = document.createElement('tr');
+    
+    let title = document.createElement('th');
+    let titleTextNode = document.createTextNode('');
+    title.contentEditable = 'true';
+    title.appendChild(titleTextNode);
+    event.appendChild(title);
+
+    let time = document.createElement('td');
+    let timeTextNode = document.createTextNode('');
+    time.contentEditable = 'true';
+    time.appendChild(timeTextNode);
+    event.appendChild(time);
+
+    let placing = document.createElement('td');
+    placing.contentEditable = 'true';
+    placing.appendChild(document.createTextNode(''));
+    
+    event.appendChild(placing);
+    table.appendChild(event);
 });
 
 socket.on('infoReturn', (json) => {
@@ -40,7 +64,7 @@ socket.on('infoReturn', (json) => {
             placing.appendChild(document.createTextNode(dataObject.placing));
         }
         else {
-            placing.appendChild(document.createTextNode("Event not finished or Error"));
+            placing.appendChild(document.createTextNode('Event not finished or Error'));
         }
         event.appendChild(placing);
         table.appendChild(event);
